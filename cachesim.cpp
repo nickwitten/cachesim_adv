1308e4e1588a6c086d0b8b91afe50a3a6d250b8e
Nick Witten <nwitt12@gmail.com>
2023-02-15 12:27:07 -0500

    first crack at vipt

--- cachesim.cpp
+++ cachesim.cpp
@@ -13,17 +13,37 @@
 */
 
 void legalize_s(sim_config_t *config) {
-
+    if (config->c - config->s > config->p) {
+        config->s = config->c - config->p;
+    }
 }
 
 
 // Valid bit is not included because the allocation of this structure indicates
-// the address is in the cache
+// the corresponding block is in the cache
 struct tag {
     char dirty;
     uint64_t value;
     struct tag *next;
 };
+
+struct translation {
+    uint64_t vpn;
+    uint64_t pfn;
+    char valid;
+    struct translation *next;
+    struct translation *prev;
+    // Parent is corresponding translation in hwivpt for tlb, not used by hwivpt
+    struct translation *parent;
+};
+
+struct translation_storage {
+    struct translation *mru;
+    struct translation *lru;
+} tlb, hwivpt;
+// All of the translations will be allocated in a block at these locations
+struct translation *tlb_translations;
+struct translation *hwivpt_translations;
 
 struct set {
     struct tag *mru;
@@ -35,13 +55,133 @@ struct tag_store {
 
 int num_ways;
 int num_sets;
+int num_pages;
+int num_tlb_entries;
+bool vipt;
 int s;
 uint64_t index_mask;
-uint64_t tag_mask;
-uint64_t offset_mask;
 uint64_t index_position;
+uint64_t tag_mask;
 uint64_t tag_position;
+uint64_t offset_mask;
 uint64_t offset_position;
+uint64_t vpn_mask;
+uint64_t vpn_position;
+
+
+struct translation *initialize_translation_storage(struct translation_storage *store, uint64_t size) {
+        // unlike cache, allocate all tlb and hwivpt entries
+        struct translation *translations_base = (struct translation *)calloc(size, sizeof(struct translation));
+        for (uint64_t i = 0; i < (size - 1); i++) {
+            translations_base[i].pfn = i;
+            translations_base[i].next = &(translations_base[i + 1]);
+            translations_base[i + 1].prev = &(translations_base[i]);
+        }
+        store->mru = translations_base;
+        store->lru = &(translations_base[size - 1]);
+        store->lru->pfn = size - 1;
+        return translations_base;
+}
+
+struct translation *search_for_translation(struct translation_storage *store, uint64_t vpn) {
+    struct translation *mapping = store->mru;
+    while (mapping != 0) {
+        if (mapping->vpn == vpn) {
+            return mapping;
+        }
+        mapping = mapping->next;
+    }
+    return 0;
+}
+
+void update_translation_mru(struct translation_storage *store, struct translation *mapping) {
+    if (mapping == 0) {
+        return;
+    }
+    if (mapping->prev == 0) {
+        return;  // Already in the MRU position
+    }
+    // Remove from current position
+    mapping->prev->next = mapping->next;
+    if (mapping->next != 0) {
+        mapping->next->prev = mapping->prev;
+    }
+    // Place at mru
+    mapping->next = store->mru;
+    mapping->prev = 0;
+    store->mru = mapping;
+}
+
+
+void insert_translation(struct translation_storage *store, int64_t vpn, int64_t pfn, struct translation *parent) {
+    struct translation *victim = store->lru;
+    victim->valid = 1;
+    victim->vpn = vpn;
+    victim->pfn = pfn;
+    victim->parent = parent;
+    if (victim->prev != 0) {
+        store->lru = victim->prev;
+    }
+    update_translation_mru(store, victim);
+}
+
+int64_t search_tlb(uint64_t addr) {
+    int64_t vpn = (addr & vpn_mask) >> vpn_position;
+    int64_t pfn = -1;
+    struct translation *mapping = search_for_translation(&tlb, vpn);
+    if (mapping == 0) {
+        // Translation wasn't found
+        return -1;
+    }
+    pfn = mapping->pfn;
+    // Update lru stack in both tlb and hwivpt
+    update_translation_mru(&tlb, mapping);
+    update_translation_mru(&hwivpt, mapping->parent);
+    return pfn;
+}
+
+int64_t search_hwivpt(uint64_t addr) {
+    int64_t vpn = (addr & vpn_mask) >> vpn_position;
+    int64_t pfn = -1;
+    struct translation *hwivpt_mapping = search_for_translation(&hwivpt, vpn);
+    if (hwivpt_mapping == 0) {
+        // Translation wasn't found
+        return -1;
+    }
+    pfn = hwivpt_mapping->pfn;
+    // Insert translation into tlb
+    // Update lru stack in both tlb and hwivpt
+    update_translation_mru(&hwivpt, hwivpt_mapping);
+    insert_translation(&tlb, vpn, pfn, hwivpt_mapping);
+    return pfn;
+}
+
+int64_t page_fault_handler(uint64_t addr) {
+    int64_t vpn = (addr & vpn_mask) >> vpn_position;
+    insert_translation(&hwivpt, vpn, hwivpt.lru->pfn, 0);
+    int64_t pfn = hwivpt.mru->pfn;
+    insert_translation(&tlb, vpn, pfn, hwivpt.mru);
+    return pfn;
+}
+
+void flush_cache(sim_stats_t *stats) {
+    for (int i = 0; i < num_sets; i++) {
+        struct tag *active_way = tag_store.sets[i]->mru;
+        while (active_way != 0) {
+            if (stats) {
+                if (active_way->dirty) {
+                    stats->cache_flush_writebacks++;
+                }
+            }
+            struct tag *next = active_way->next;
+            free(active_way);
+            active_way = next;
+        }
+        tag_store.sets[i]->mru = 0;
+    }
+}
+
+
 
 /**
  * Subroutine for initializing the cache simulator. You many add and initialize any global or heap
@@ -70,7 +210,19 @@ void sim_setup(sim_config_t *config) {
 
     tag_position = config->c - config->s;
     tag_mask = ~0 & ~(offset_mask | index_mask);
+
+    if (config->vipt) {
+        vipt = true;
+        num_pages = 1 << config->m;
+        num_tlb_entries = 1 << config->t;
+        tlb_translations = initialize_translation_storage(&tlb, num_tlb_entries);
+        hwivpt_translations = initialize_translation_storage(&hwivpt, num_pages);
+        // address masks and positions
+        vpn_mask = ~((1 << config->p) - 1);
+        vpn_position = config->p;
+    }
 }
+
 
 /**
  * Subroutine that simulates the cache one trace event at a time.
@@ -82,30 +234,64 @@ void sim_access(char rw, uint64_t addr, sim_stats_t* stats) {
     uint64_t tag = (addr & tag_mask) >> tag_position;
     stats->array_lookups_l1++;
     struct set *active_set = tag_store.sets[index];
-    struct tag *active_way = active_set->mru;
-    struct tag *prev_way = 0;
-    int hit_flag = 0;
-    stats->tag_compares_l1 += num_ways;
-    while (active_way != 0) {
-        if (tag == active_way->value) {
-            // HIT!!
-            stats->hits_l1++;
-            hit_flag = 1;
-            // Remove from current position
-            if (prev_way == 0) {
-                // Hit on MRU
-                active_set->mru = active_way->next;
-            } else {
-                prev_way->next = active_way->next;
-            }
-            break;
+    struct tag *active_way;
+    int64_t pfn = -1;
+
+    if (vipt) {
+        stats->accesses_tlb++;
+        pfn = search_tlb(addr);
+        if (pfn < 0) {
+            // Translation not in TLB
+            stats->misses_tlb++;
+        } else {
+            stats->hits_tlb++;
+            addr = (pfn << vpn_position) | (addr & ~vpn_mask);
         }
-        prev_way = active_way;
-        active_way = active_way->next;
+    }
+
+    int hit_flag = 0;
+    if (!vipt || pfn >= 0) {
+        active_way = active_set->mru;
+        struct tag *prev_way = 0;
+        stats->tag_compares_l1 += num_ways;
+        while (active_way != 0) {
+            if (tag == active_way->value) {
+                // HIT!!
+                stats->hits_l1++;
+                hit_flag = 1;
+                // Remove from current position
+                if (prev_way == 0) {
+                    // Hit on MRU
+                    active_set->mru = active_way->next;
+                } else {
+                    prev_way->next = active_way->next;
+                }
+                break;
+            }
+            prev_way = active_way;
+            active_way = active_way->next;
+        }
+        if (!hit_flag) {
+            stats->misses_l1++;
+        }
+    }
+
+    if (vipt && pfn < 0) {
+        stats->accesses_hw_ivpt++;
+        pfn = search_hwivpt(addr);
+        if (pfn < 0) {
+            // Page fault!!
+            stats->misses_hw_ivpt++;
+            flush_cache(stats);
+            pfn = page_fault_handler(addr);
+        } else {
+            stats->hits_hw_ivpt++;
+        }
+        addr = (pfn << vpn_position) | (addr & ~vpn_mask);
+        tag = (addr & tag_mask) >> tag_position;
     }
 
     if (!hit_flag) {
-        stats->misses_l1++;
         // Allocate a new tag
         active_way = (struct tag *)calloc(1, sizeof(struct tag));
         active_way->value = tag;
@@ -151,13 +337,13 @@ void sim_finish(sim_stats_t *stats) {
         L1_TAG_COMPARE_TIME_CONST + s * L1_TAG_COMPARE_TIME_PER_S);
     stats->avg_access_time = hit_time + stats->miss_ratio_l1 * DRAM_ACCESS_PENALTY;
     // Free the tag store
+    flush_cache(0);
     for (int i = 0; i < num_sets; i++) {
-        struct tag *active_way = tag_store.sets[i]->mru;
-        while (active_way != 0) {
-            struct tag *next = active_way->next;
-            free(active_way);
-            active_way = next;
-        }
         free(tag_store.sets[i]);
+    }
+    if (vipt) {
+        // Free the virtual address translations
+        free(tlb_translations);
+        free(hwivpt_translations);
     }
 }
